396b5369f2535ad2ec59fd5e5a27af4798fc1b55
Johanne Boisvilliers <johanne.boisvilliers@gmail.com>
2018-10-23 22:26:59 +0200

    base repository with basic connection to firebase

---
+++ app/src/main/java/com/example/jbois/go4lunch/Controllers/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.jbois.go4lunch.Controllers;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.jbois.go4lunch.R;
+import com.firebase.ui.auth.AuthUI;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.startSignInActivity();
+    }
+
+    private void startSignInActivity(){
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setTheme(R.style.LoginTheme)
+                        .setAvailableProviders(
+                                Arrays.asList(new AuthUI.IdpConfig.GoogleBuilder().build()))
+                        .setIsSmartLockEnabled(false, true)
+                        //.setLogo(R.drawable.ic_logo_auth)
+                        .build(),
+                RC_SIGN_IN);
+    }
+}
