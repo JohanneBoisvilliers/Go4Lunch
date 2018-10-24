08ef98b987c166f1c84c92327709a78418c37a34
jograph <johanne.boisvilliers@gmail.com>
2018-10-24 16:41:36 +0200

    Now when user is connect, a new window is openning: the futur activity which will contain the main viewpager

--- app/src/main/java/com/example/jbois/go4lunch/Controllers/MainActivity.java
+++ app/src/main/java/com/example/jbois/go4lunch/Controllers/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.jbois.go4lunch.Controllers;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+
 
 import com.example.jbois.go4lunch.R;
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
 
 import java.util.Arrays;
 
+import butterknife.BindView;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.main_activity_coordinator_layout) CoordinatorLayout mCoordinatorLayout;
 
     private static final int RC_SIGN_IN = 123;
 
@@ -20,6 +30,14 @@ public class MainActivity extends AppCompatActivity {
         this.startSignInActivity();
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // 4 - Handle SignIn Activity response on activity result
+        this.handleResponseAfterSignIn(requestCode, resultCode, data);
+        this.startLunchActivity();
+    }
+    //Start an activity that let user choose between two ways for connection
     private void startSignInActivity(){
         startActivityForResult(
                 AuthUI.getInstance()
@@ -31,5 +49,32 @@ public class MainActivity extends AppCompatActivity {
                         //.setLogo(R.drawable.ic_logo_auth)
                         .build(),
                 RC_SIGN_IN);
+    }
+    //show a snackbar into the bottom of screen to say that the connection work well
+    private void showSnackBar(CoordinatorLayout coordinatorLayout, String message){
+        Snackbar.make(coordinatorLayout, message, Snackbar.LENGTH_SHORT).show();
+    }
+    //catch if there are any problems when user try to connect
+    private void handleResponseAfterSignIn(int requestCode, int resultCode, Intent data){
+
+        IdpResponse response = IdpResponse.fromResultIntent(data);
+
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) { // SUCCESS
+            } else { // ERRORS
+                if (response == null) {
+                    showSnackBar(this.mCoordinatorLayout, getString(R.string.error_authentication_canceled));
+                } else if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    showSnackBar(this.mCoordinatorLayout, getString(R.string.error_no_internet));
+                } else if (response.getError().getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    showSnackBar(this.mCoordinatorLayout, getString(R.string.error_unknown_error));
+                }
+            }
+        }
+    }
+    //start the LunchActivity after user's connection
+    private void startLunchActivity(){
+        Intent intent = new Intent(this, LunchActivity.class);
+        startActivity(intent);
     }
 }
